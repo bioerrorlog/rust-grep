0b1f712c1ff64c3fa006738bd1934c05e4de045a
bioerrorlog <n.iijima.0x01@gmail.com>
2022-02-21 09:06:37 +0900

    apply clap

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
     pattern: String,
+    #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
-    println!("Hello, world! {}, {}", args.pattern);
+    let args = Cli::parse();
 }

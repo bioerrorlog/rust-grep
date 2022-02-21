812783af774eb08d1d95611e6d119c68b5a1a392
bioerrorlog <n.iijima.0x01@gmail.com>
2022-02-21 20:36:51 +0900

    add simple test

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,21 @@ fn main() -> Result<()>{
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
 
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
         }
     }
-    Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("aaa bbb\nccc ddd eee", "bbb", &mut result);
+    assert_eq!(result, b"aaa bbb\n");
 }

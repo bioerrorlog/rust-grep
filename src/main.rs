d81c3b8b85855f361391906391f9e617954b6054
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-10-23 12:18:00 +0900

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
